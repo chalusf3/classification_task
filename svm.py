86752c14a94df826a6ce67094a19b78df5f651a3
François Chalus <chalusf3@gmail.com>
2018-07-19 11:45:27 +0100

    modified svm trainning to use LinearSVC when using feature vectors

---
+++ svm.py
@@ -0,0 +1,26 @@
+from sklearn import svm
+import numpy as np
+
+def fit_svm_from_kernel(K, y_train, K_pred):
+    clf = svm.SVC(kernel = 'precomputed')
+    
+    clf.fit(K, y_train)
+    return clf.predict(K_pred)
+
+def fit_from_feature_gen(X_train, y_train, X_pred, C, feature_gen):
+    PhiX_train = feature_gen(X_train)
+    PhiX_pred = feature_gen(X_pred)
+    
+    clf = svm.LinearSVC(dual = (PhiX_train.shape[0] < PhiX_train.shape[1]))
+    clf.fit(PhiX_train, y_train)
+
+    return clf.predict(PhiX_pred)
+
+def fit_from_kernel_gen(X_train, y_train, X_pred, C, kernel_gen):
+    K = kernel_gen(X_train, X_train)
+    K_pred = kernel_gen(X_pred, X_train)
+    
+    clf = svm.SVC(kernel = 'precomputed')
+    
+    clf.fit(K, y_train)
+    return clf.predict(K_pred)
