e8ee8bc34a0a2fef2b03b125337f337639b355f0
François Chalus <chalusf3@gmail.com>
2018-07-19 16:02:47 +0100

    added the regularization parameter C which was not used previously

--- svm.py
+++ svm.py
@@ -11,7 +11,7 @@ def fit_from_feature_gen(X_train, y_train, X_pred, C, feature_gen):
     PhiX_train = feature_gen(X_train)
     PhiX_pred = feature_gen(X_pred)
     
-    clf = svm.LinearSVC(dual = (PhiX_train.shape[0] < PhiX_train.shape[1]), verbose = False)
+    clf = svm.LinearSVC(dual = (PhiX_train.shape[0] < PhiX_train.shape[1]), C = C, verbose = False)
     clf.fit(PhiX_train, y_train)
 
     return clf.predict(PhiX_pred)
@@ -20,6 +20,6 @@ def fit_from_kernel_gen(X_train, y_train, X_pred, C, kernel_gen):
     K = kernel_gen(X_train, X_train)
     K_pred = kernel_gen(X_pred, X_train)
     
-    clf = svm.SVC(kernel = kernel_gen, verbose = False)
+    clf = svm.SVC(kernel = kernel_gen, C = C, verbose = False)
     clf.fit(K, y_train)
     return clf.predict(K_pred)
